0551a7bbbd4869df04f184046e269dfb0da77984
Jonathan Morton <jon.morton@gmail.com>
2016-11-10 09:18:24 -0600

    Update project metadata and dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject uberconf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Configuration made Über."
+  :url "http://github.com/jmorton/uberconf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [clojure-ini "0.0.2"]
+                 [prismatic/schema "1.1.3"]
+                 [camel-snake-kebab "0.4.0"]])
